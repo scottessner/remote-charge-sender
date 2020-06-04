3e9e1c7cb4d7590264054117fc1a08dac6cab87d
Scott Essner <scottessner@gmail.com>
2020-06-04 14:21:08 +0000

    Added timeout and changed to paho

--- charger.py
+++ charger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 ########################################################################
@@ -23,20 +23,30 @@ import sys
 import time
 import serial
 import json
-import pika
+import paho.mqtt.client as mqtt
+import logging
+
+logging.basicConfig(filename='/var/tmp/charger.log', filemode='w', level=logging.DEBUG)
+
+mode = 0 
+
+client = mqtt.Client()
+
+def set_mode(new_mode):
+
+    print('New mode: {0}'.format(mode))
+    global mode 
+    # if new_mode != mode:
+    mode = new_mode
+    logging.info('New mode: {0}'.format(mode))
+    client.publish('battery_charger/mode_string', mop[mode], 2, True)    
+    client.publish('battery_charger/mode_int', mode, 2, True)    
 
 charger_status = [None]*4
 
 charger_status[0] = 'No Charger Connected'
 charger_status[1] = 'Charger Idle'
 charger_status[2] = 'Charger Active'
-
-creds = pika.PlainCredentials('charger', 'charger')
-connection = pika.BlockingConnection(pika.ConnectionParameters('ssessner.com', credentials=creds))
-
-channel = connection.channel()
-
-channel.queue_declare(queue='batt')
  
 # discharge end voltage (empty)
  
@@ -46,6 +56,7 @@ dchg_end = 3.2
 # iCharger modes of operation
 mop = [None]*15
  
+mop[0] = "Startup"
 mop[1] = "Charging"
 mop[2] = "Discharging"
 mop[3] = "Monitor"
@@ -61,16 +72,27 @@ mop[12] = "External-discharging"
 mop[13] = "Not Connected"
 mop[14] = "Idle"
  
+#cell_numbers
+cells = dict()
+cells[6] = 'cell_1_voltage'
+cells[7] = 'cell_2_voltage'
+cells[8] = 'cell_3_voltage'
+cells[9] = 'cell_4_voltage'
+cells[10] = 'cell_5_voltage'
+cells[11] = 'cell_6_voltage'
+
 # configure the serial connections 
 # change according to the ttyUSB assigned to the iCharger (dmesg)
  
+client.connect('ssessner.noip.us', keepalive=30)
+
 while 1:
 
     state = dict()
 
     try:
 
-        ser = serial.Serial('/dev/charger', baudrate=230400, timeout=5)
+        ser = serial.Serial('/dev/charger', baudrate=230400, timeout=15)
 
         # ser.open()
         ser.isOpen()
@@ -80,40 +102,34 @@ while 1:
 
         if len(line) > 0:
 
+            logging.debug(line)
             raw = line.split(';')
             length = len(raw)
+            logging.info('Length: {}'.format(length))
 
-            state['mode'] = mop[int(raw[1])]
-            state['state'] = dict()
-            state['state']['input_voltage'] = float(raw[3]) / 1000
-            state['state']['battery_voltage'] = float(raw[4]) / 1000
-            state['state']['charge_current'] = int(raw[5]) * 10
-            state['state']['cell_voltages'] = list()
+            set_mode(int(raw[1]))
+            state['input_voltage'] = float(raw[3]) / 1000
+            state['pack_voltage'] = float(raw[4]) / 1000
+            state['charge_current'] = int(raw[5]) * 10
 
             for cell in range(6, length-5):
                 if raw[cell] != '0':
-                    state['state']['cell_voltages'].insert(cell - 6, float(raw[cell]) / 1000)
+                    state[cells[cell]] = float(raw[cell]) / 1000
 
-            state['state']['internal_temp'] = float(raw[length - 4]) / 10
-            state['state']['external_temp'] = float(raw[length - 3]) / 10
-            state['state']['total_charge'] = int(raw[length - 2])
+            state['internal_temp'] = round((float(raw[length - 4]) / 10)*9/5 + 32, 0)
+            state['external_temp'] = round((float(raw[length - 3]) / 10)*9/5 + 32, 0)
+            state['total_charge'] = int(raw[length - 2])
 
-            # message = json.dumps(state)
-            #channel.basic_publish(exchange='amq.fanout',
-            #                      routing_key='battery',
-            #                      body=message
-            #                      )
+            for key in state.keys():
+                logging.debug('Sending {0} to topic battery_charger/{1}'.format(state[key], key))
+                client.publish('battery_charger/{0}'.format(key), state[key])
 
         else:
-            state['mode'] = mop[14]
+            logging.debug('Empty Serial Line')
+            set_mode(14)
 
     except serial.SerialException as ex:
-
-        state['mode'] = mop[13]
+        logging.error(ex)
+        set_mode(13)
         time.sleep(5)
 
-    message = json.dumps(state)
-    channel.basic_publish(exchange='amq.fanout',
-                          routing_key='battery',
-                          body=message
-                          )
